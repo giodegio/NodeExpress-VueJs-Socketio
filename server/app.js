990e398f0b697a05efbd542ee7b1a1c4b3022a41
Giorgio Degioanni <gio.degio@gmail.com>
2018-06-27 14:30:01 +0200

    setting up the base project

--- server/app.js
+++ server/app.js
@@ -14,13 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../client/dist/static/img', 'degio.ico')));
+
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/', index);
 app.use('/users', users);
